dcb3d9b24dd7c9e142696e92554aeff5083da718
hahahajin <yhj010810@naver.com>
2021-05-17 22:08:01 +0900

    Update views.py

--- BoardGameGuide/boardgame/views.py
+++ BoardGameGuide/boardgame/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def main(request):
+    return render(request, 'main.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def mypage(request):
+    return render(request, 'mypage.html')
