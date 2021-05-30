e4fa2420857557917c250f147fa834d58d7a7311
kimhyeji-gg <wl159753@naver.com>
2021-05-31 02:18:51 +0900

    Create comu_update

--- BoardGameGuide/BoardGameGuide/urls.py
+++ BoardGameGuide/BoardGameGuide/urls.py
@@ -19,6 +19,8 @@ from django.urls import path
 import boardgame.views
 
 urlpatterns = [
+    path('comu_list/<int:community_id>/comu_update/', boardgame.views.comu_update, name = 'comu_update'),
+
     path('create/', boardgame.views.create, name = "create"),
     path('comu_list/<int:community_id>', boardgame.views.comu_detail, name = 'comu_detail'),
     path('comu_write/', boardgame.views.comu_write, name = 'comu_write'),
